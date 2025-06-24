84da93f9ca1a83f7c0daa1af5b67be21c4cdec50
TechTitans-Academy <learnwithtechtitans@gmail.com>
2025-06-24 12:07:57 +0530

    My first program

---
+++ course.py
@@ -0,0 +1,6 @@
+output = input("Are You RedHat Certified Engineer?? (yes/no)- ")
+
+if output == "yes":
+    print("Configuration, You are one step ahead");
+if output == "no":
+    print("Level Up your Linux Career with RHCSA!! ")
