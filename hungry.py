e41293c4e836161b5e3f88ab5902b770d907d3ae
TechTitans-Academy <learnwithtechtitans@gmail.com>
2025-06-24 11:53:31 +0530

    Added Lime Water Option

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hungry=="yes":
 	print ("eat Burger")
 	print ("eat Fries!!!")
 	print ("Or Eat Some Healthy Food")
+	print ("Have some Lime water")
 else:
 	thirsty=input("Are you Thirsty?")
 	if thirsty=="yes":
